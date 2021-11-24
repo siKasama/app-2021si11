f9d498ad567e0a667d027640a0a21966cd371d83
Simone Kasama Nakata <simone.ti@plasutil.com.br>
2021-11-24 16:28:39 -0300

    primeiro commit - criando migration

---
+++ database/migrations/2021_11_24_191338_create_produtos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id()->from(1);
+            $table->timestamps();
+            $table->string('nome');
+            $table->string('SKU');
+            $table->bigInteger('quantidade_inicial');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
